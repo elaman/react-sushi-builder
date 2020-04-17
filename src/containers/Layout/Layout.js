1d070699d768e482a69fc8feaf5c09e51e0984f6
Elaman Imashov <elaman@outlook.com>
2020-04-17 11:48:19 +0600

    Add DrawerToggle component to open Drawer

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -4,7 +4,7 @@ import Drawer from "../../components/Layout/Drawer/Drawer";
 import classes from "./Layout.module.css";
 
 export default ({ children }) => {
-  const [drawerOpen, setDrawerOpen] = useState(true);
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   function toggleDrawer() {
     setDrawerOpen(!drawerOpen);
@@ -12,7 +12,7 @@ export default ({ children }) => {
 
   return (
     <div className={classes.Layout}>
-      <Toolbar />
+      <Toolbar toggleDrawer={toggleDrawer} />
       <Drawer open={drawerOpen} toggleDrawer={toggleDrawer} />
       <main>{children}</main>
     </div>
