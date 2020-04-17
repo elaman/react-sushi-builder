7d1e265b6eef28b046981ada565f17eade42752f
Elaman Imashov <elaman@outlook.com>
2020-04-17 10:49:46 +0600

    Add and stylize Drawer component

--- src/components/Layout/Drawer/Drawer.js
+++ src/components/Layout/Drawer/Drawer.js
@@ -2,5 +2,7 @@ import React from 'react';
 import classes from './Drawer.module.css';
 
 export default () => (
-  <div className={classes.Drawer}>Drawer</div>
+  <div className={classes.Drawer}>
+    Drawer
+  </div>
 );
