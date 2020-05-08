ffa4a665301e7142a479e834c4aff1332cf795f6
Elaman Imashov <elaman@outlook.com>
2020-05-08 09:48:55 +0600

    Add button event listeners

--- src/components/Checkout/CheckoutSummary/CheckoutSummary.js
+++ src/components/Checkout/CheckoutSummary/CheckoutSummary.js
@@ -3,12 +3,16 @@ import SushiKit from "../../SushiBuilder/SushiKit/SushiKit";
 import Button from "../../UI/Button/Button";
 import classes from "./CheckoutSummary.module.css";
 
-export default ({ price, ingredients }) => {
+export default ({ price, ingredients, checkoutCancel, checkoutContinue }) => {
   return (
     <div className={classes.CheckoutSummary}>
       <SushiKit price={price} ingredients={ingredients} />
-      <Button red>Cancel</Button>
-      <Button green>Continue</Button>
+      <Button click={checkoutCancel} red>
+        Cancel
+      </Button>
+      <Button click={checkoutContinue} green>
+        Continue
+      </Button>
     </div>
   );
 };
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import CheckoutSummary from "../../components/Checkout/CheckoutSummary/CheckoutSummary";
 import classes from "./Checkout.module.css";
 
 export default () => {
+  const history = useHistory();
   const ingredients = {
     avocadoMaki: 1,
     avocadoTunaRoll: 1,
@@ -13,9 +15,22 @@ export default () => {
   };
   const price = 123;
 
+  function checkoutCancel() {
+    history.push("/builder");
+  }
+
+  function checkoutContinue() {
+    history.push("/checkout/finish");
+  }
+
   return (
     <div className={classes.Checkout}>
-      <CheckoutSummary ingredients={ingredients} price={price} />
+      <CheckoutSummary
+        ingredients={ingredients}
+        price={price}
+        checkoutCancel={checkoutCancel}
+        checkoutContinue={checkoutContinue}
+      />
     </div>
   );
 };
