67fb43b1405a3f8b6d39f575d8f95a6ce8540b92
Elaman Imashov <elaman@outlook.com>
2020-05-08 10:19:03 +0600

    Send ingredients to checkout

--- src/containers/SushiBuilder/SushiBuilder.js
+++ src/containers/SushiBuilder/SushiBuilder.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "../../axios";
 import SushiKit from "../../components/SushiBuilder/SushiKit/SushiKit";
-import classes from "./SushiBuilder.module.css";
 import SushiControls from "../../components/SushiBuilder/SushiControls/SushiControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/SushiBuilder/OrderSummary/OrderSummary";
-import axios from "../../axios";
 import Spinner from "../../components/UI/Spinner/Spinner";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
+import classes from "./SushiBuilder.module.css";
 
 const PRICES = {
   avocadoMaki: 6.9,
@@ -23,6 +24,7 @@ export default withErrorHandler(() => {
   const [canOrder, setCanOrder] = useState(false);
   const [isOrdering, setIsOrdering] = useState(false);
   const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
   function checkCanOrder(ingredients) {
     const total = Object.keys(ingredients).reduce((total, ingredient) => {
@@ -40,24 +42,17 @@ export default withErrorHandler(() => {
   }
 
   function finishOrder() {
-    const order = {
-      ingredients: ingredients,
-      price: price,
-      delivery: "Fast",
-      customer: {
-        name: "Bakyt",
-        phone: "0700700700",
-        address: {
-          street: "123 Gebze",
-          city: "Karakol",
-        },
-      },
-    };
+    const queryParams = Object.keys(ingredients).map(
+      (ingredient) =>
+        encodeURIComponent(ingredient) +
+        "=" +
+        encodeURIComponent(ingredients[ingredient])
+    );
+    queryParams.push("price=" + encodeURIComponent(price.toFixed(2)));
 
-    setLoading(true);
-    axios.post("/orders.json", order).then((response) => {
-      setLoading(false);
-      setIsOrdering(false);
+    history.push({
+      pathname: "/checkout",
+      search: queryParams.join("&"),
     });
   }
 
