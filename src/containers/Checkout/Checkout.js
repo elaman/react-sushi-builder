3e12c47636ba922c85028dc498bef927c6df6af8
Elaman Imashov <elaman@outlook.com>
2020-05-13 09:41:47 +0600

    Add axios to checkout

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,12 +5,14 @@ import CheckoutSummary from "../../components/Checkout/CheckoutSummary/CheckoutS
 import classes from "./Checkout.module.css";
 import CheckoutForm from "./CheckoutForm/CheckoutForm";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
 export default withErrorHandler(() => {
   const history = useHistory();
   const location = useLocation();
   const [ingredients, setIngredients] = useState({});
   const [price, setPrice] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const query = new URLSearchParams(location.search);
@@ -33,7 +35,24 @@ export default withErrorHandler(() => {
     history.push("/checkout/form");
   }
 
-  function checkoutFinish(data) {}
+  function checkoutFinish(data) {
+    setLoading(true);
+    axios
+      .post("/orders.json", {
+        ingredients,
+        price,
+        details: data,
+      })
+      .then((response) => {
+        setLoading(false);
+        history.replace("/");
+      });
+  }
+
+  let formOutput = <Spinner />;
+  if (!loading) {
+    formOutput = <CheckoutForm checkoutFinish={checkoutFinish} />;
+  }
 
   return (
     <div className={classes.Checkout}>
@@ -43,9 +62,7 @@ export default withErrorHandler(() => {
         checkoutCancel={checkoutCancel}
         checkoutContinue={checkoutContinue}
       />
-      <Route path="/checkout/form">
-        <CheckoutForm checkoutFinish={checkoutFinish} />
-      </Route>
+      <Route path="/checkout/form">{formOutput}</Route>
     </div>
   );
 }, axios);
