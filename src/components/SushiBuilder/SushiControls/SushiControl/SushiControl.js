6aa61005e3d30452fca9bd38eb50ade53b1796b4
Elaman Imashov <elaman@outlook.com>
2020-05-22 09:35:36 +0600

    Use dispatcher

--- src/components/SushiBuilder/SushiControls/SushiControl/SushiControl.js
+++ src/components/SushiBuilder/SushiControls/SushiControl/SushiControl.js
@@ -1,21 +1,27 @@
 import React from "react";
 import classes from "./SushiControl.module.css";
+import { useDispatch } from "react-redux";
+import { ADD_INGREDIENT, REMOVE_INGREDIENT } from "../../../../store/actions";
 
-export default ({ control, removeIngredient, addIngredient, disabled }) => (
-  <div className={classes.SushiControl}>
-    <button
-      className={classes.less}
-      onClick={() => removeIngredient(control.type)}
-      disabled={disabled}
-    >
-      -
-    </button>
-    <span className={classes.label}>{control.label}</span>
-    <button
-      className={classes.more}
-      onClick={() => addIngredient(control.type)}
-    >
-      +
-    </button>
-  </div>
-);
+export default ({ control, removeIngredient, addIngredient, disabled }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className={classes.SushiControl}>
+      <button
+        className={classes.less}
+        onClick={() => dispatch({ type: REMOVE_INGREDIENT })}
+        disabled={disabled}
+      >
+        -
+      </button>
+      <span className={classes.label}>{control.label}</span>
+      <button
+        className={classes.more}
+        onClick={() => dispatch({ type: ADD_INGREDIENT })}
+      >
+        +
+      </button>
+    </div>
+  );
+};
