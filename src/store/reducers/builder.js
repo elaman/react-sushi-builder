593bbc662b38773cbe71e4fd67233cc596509aac
Elaman Imashov <elaman@outlook.com>
2020-05-29 09:15:53 +0600

    Restructure store files

--- src/store/reducers/builder.js
+++ src/store/reducers/builder.js
@@ -1,4 +1,4 @@
-import * as actions from "./actions";
+import * as types from "../actions/types";
 
 const initialState = {
   ingredients: {
@@ -23,7 +23,7 @@ const PRICES = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case actions.ADD_INGREDIENT:
+    case types.ADD_INGREDIENT:
       return {
         ...state,
         ingredients: {
@@ -33,7 +33,7 @@ export default (state = initialState, action) => {
         price: state.price + PRICES[action.ingredient],
       };
 
-    case actions.REMOVE_INGREDIENT:
+    case types.REMOVE_INGREDIENT:
       return {
         ...state,
         ingredients: {
