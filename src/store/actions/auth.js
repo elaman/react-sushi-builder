55d5bc9440af1588856f23e4b2e2cc0a2b7c6ce8
Elaman Imashov <elaman@outlook.com>
2020-06-03 10:07:36 +0600

    Add Firebase URL

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -13,6 +13,7 @@ export const fail = (dispatch, errors) => dispatch({
   type: AUTH_FAIL, errors
 });
 
-export const auth = (dispatch, email, password) => axios.post()
+export const auth = (dispatch, email, password) => axios
+  .post("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCrbFZwYZA20UbJRbWyM4rA25uOO-u3zXA", { email, password })
   .then(({ data }) => success(dispatch, data))
   .catch(error => fail(dispatch, error));
