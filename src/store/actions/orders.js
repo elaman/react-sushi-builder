4726b2b3a89a5e10de7af82f8bc2b3fc24a66570
Elaman Imashov <elaman@outlook.com>
2020-06-05 10:17:05 +0600

    Add orders authentication

--- src/store/actions/orders.js
+++ src/store/actions/orders.js
@@ -5,7 +5,7 @@ export const set = (dispatch, orders) => dispatch({
   type: SET_ORDERS, orders
 });
 
-export const load = (dispatch) => axios
-  .get("/orders.json")
+export const load = (dispatch, token) => axios
+  .get("/orders.json?auth=" + token)
   .then(({ data }) => set(dispatch, data))
   .catch(() => {});
