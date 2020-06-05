9e0790f007aba39e1b9726ac42e97d3137d83e83
Elaman Imashov <elaman@outlook.com>
2020-06-05 11:24:51 +0600

    Restrict access to routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { restore } from "./store/actions/auth";
 import Layout from "./containers/Layout/Layout";
 import SushiBuilder from "./containers/SushiBuilder/SushiBuilder";
 import Checkout from "./components/Checkout/Checkout";
@@ -7,38 +9,57 @@ import Orders from "./components/Orders/Orders";
 import Auth from "./components/Auth/Auth";
 import Logout from "./components/Logout/Logout";
 import "./App.css";
-import { restore } from "./store/actions/auth";
-import { useDispatch } from "react-redux";
 
 export default () => {
   const dispatch = useDispatch();
+  const isAuthenticated = useSelector(state => state.auth.token !== null);
   useEffect(() => {
     restore(dispatch);
   }, [dispatch]);
 
+  let routesOutput = (
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/builder" />
+      </Route>
+      <Route path="/builder">
+        <SushiBuilder />
+      </Route>
+      <Route path="/auth">
+        <Auth />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  );
+  if (isAuthenticated) {
+    routesOutput = (
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/builder" />
+        </Route>
+        <Route path="/builder">
+          <SushiBuilder />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="/orders">
+          <Orders />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Route path="/logout">
+          <Logout />
+        </Route>
+      </Switch>
+    );
+  }
+
   return (
     <div className="App">
       <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/builder" />
-          </Route>
-          <Route path="/builder">
-            <SushiBuilder />
-          </Route>
-          <Route path="/checkout">
-            <Checkout />
-          </Route>
-          <Route path="/orders">
-            <Orders />
-          </Route>
-          <Route path="/auth">
-            <Auth />
-          </Route>
-          <Route path="/logout">
-            <Logout />
-          </Route>
-        </Switch>
+        {routesOutput}
       </Layout>
     </div>
   );
